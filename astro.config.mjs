854fc99cbb1e6dcc19fb63331038aeada49feb3a
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-04 12:15:08 +0200

    testes gith

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
+  site: 'https://mariamsanoko.github.io/formationia/',
   base: '/astro-ecommerce'
 });
