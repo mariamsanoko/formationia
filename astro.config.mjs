d24814effb9da9f0f62f101ffa81dcf763429e25
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-04 13:22:41 +0200

    cor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import staticAdapter from '@astrojs/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: staticAdapter(),
   site: 'https://mariamsanoko.github.io/formationia',
-  base: '/formationia'
+  base: '/formationia/',
+  output: 'static',
 });
