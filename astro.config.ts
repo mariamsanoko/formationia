35d273c0fb876bee13d4d9e9d2ee354a0e61bd88
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-19 14:17:50 +0200

    user name

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://formationia.github.io',
+  site: 'https://mariamsanoko.github.io',
   base: 'formationia',
 
   output: 'static',
