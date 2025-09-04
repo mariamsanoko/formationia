4e9bda6b64c43ffe81436f81a981c89cbe0c082f
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-04 21:10:37 +0200

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -170,13 +170,13 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/mariamsanoko"> mariamsanoko</a> · All rights reserved.
+  
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/mariamsanoko"> mariamsanoko</a> · All rights reserved 2025.
   `,
 };
