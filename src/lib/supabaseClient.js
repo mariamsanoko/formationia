744c0af96c685cb505af0cd9993e82ddced8d6c4
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-07 20:21:28 +0200

    u

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,9 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient, supabaseUrl } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_ANON_KEY
+const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey: string = process.env.REACT_APP_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
-export default supabase
+export default supabase;
