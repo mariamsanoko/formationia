489711b2ec44bfd7e3dc1943f59c95e05d227e56
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-07 21:12:36 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,17 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-/// <reference types="vite/client" />
-/// <reference types="../vendor/integration/types.d.ts" />
+// <reference path="../.astro/types.d.ts" />
+// <reference types="astro/client" />
+// <reference types="vite/client" />
+// <reference types="../vendor/integration/types.d.ts" />
 
 
 
 interface ImportMetaEnv { 
-        readonly SUPABASE_URL:string
+        readonly SUPABASE_URL: string
         readonly SUPABASE_ANON_KEY: string 
-        }
+}
 
 interface ImportMeta { 
-        readonly env: ImportMetaEnv   
-        }
+        readonly env: ImportMetaEnv
+}
       
