0fcc03380a02266b2d6755d6bb26e20af2b1d32f
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-12 16:58:24 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-// <reference path="../.astro/types.d.ts" />
-//  <reference types="astro/client" />
-// <reference types="vite/client" />
-// <reference types="../vendor/integration/types.d.ts" 
 
 interface ImportMetaEnv { 
         readonly SUPABASE_URL: string
