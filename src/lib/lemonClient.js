69b4b37712e01ca69f07b13c7744c2a38ebb3a39
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-12 16:47:31 +0200

    up

--- src/lib/lemonClient.js
+++ src/lib/lemonClient.js
@@ -1,13 +1,13 @@
-import { lemonSqueezySetup } from "@lemonsqueezy/lemonsqueezy.js";
+import { Lemonsqueezy } from '@lemonsqueezy/lemonsqueezy.js'
 
-const lemon = new lemonSqueezy(process.env.LEMON_API_KEY)
-    
+const lemon = new Lemonsqueezy(process.env.LEMON_API_KEY)
+
 export async function createCheckout() {
-        const checkout = await.lemon.checkouts.createCheckout(process.env.STORE_ID,{
-             variant_id:process.env.VARIANT_ID
-             checkout_data: {
-                email:'client@example.com',
-             },
-        })
-        return checkout.data.attributes.url
-     }
+  const checkout = await lemon.checkouts.createCheckout(process.env.STORE_ID, {
+    variant_id: process.env.VARIANT_ID, // ID de ta formation
+    checkout_data: {
+      email: 'client@example.com',
+    },
+  })
+  return checkout.data.attributes.url
+}
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,7 +1,7 @@
-import { APIRoute } from "astro";
-import { supabase } from "../../lib/supabase";
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabaseClient";
 
-export const POST: APIRoute = asynvc ({ request, redirect }) => {
+export const POST: APIRoute = async ({ request, redirect }) => {
     const formData =  await request.formData();
     const email = formData.ge("email")toString();
     const passwrd= formData.get("passwrd")toString();
