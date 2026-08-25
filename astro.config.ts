c363e137b9a9cd6494d006ea3ec64a51d62ecae7
mariamsanoko <mariam.sanoko@gmail.com>
2026-08-25 18:24:00 +0200

    Fix: désactivation stricte du prerender pour forcer le mode serveur pur

--- astro.config.ts
+++ astro.config.ts
@@ -26,7 +26,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   site: 'https://www.mariamsanoko.fr',
   base: '/',
-  output: 'server',
+  output: 'static',
 
   integrations: [
     tailwind({
